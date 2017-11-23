6d32260cb15b7c22b86a930db30345f982bdebed
Jahed <jahed.public@gmail.com>
2017-11-23 01:32:39 +0000

    fix: Can't snapshot test stack traces as they have absolute paths.

--- src/formatError.test.ts
+++ src/formatError.test.ts
@@ -2,7 +2,5 @@ import { formatError } from './formatError'
 
 test('should format error', () => {
     const result = formatError(new Error('This is the message'))
-
     expect(result).toContain('This is the message')
-    expect(result).toMatchSnapshot()
 })
